e3f707173e13e04d3b669898d1fc90532ffd05b4
Roo <rosiebrigham@gmail.com>
2017-06-14 13:54:48 +0100

    Create machete component

---
+++ components/machrie.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+class Machrie extends Component {
+
+  constructor(props){
+    super(props)
+  }
+
+  render () {
+    return (
+        <div id="machrie">
+          <p>
+            stuff about Machrie moor
+          </p>
+        </div>
+      )
+  }
+}
+
+
+export default Machrie
