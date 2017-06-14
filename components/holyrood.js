86746cdc4e78af5b36cfdd49643a2e8ee65a4bd2
Roo <rosiebrigham@gmail.com>
2017-06-14 13:54:42 +0100

    creates Holyrood component

---
+++ components/holyrood.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+class Holyrood extends Component {
+
+  constructor(props){
+    super(props)
+  }
+
+  render () {
+    return (
+        <div id="holyrood">
+          <p>
+            stuff about Holyrood
+          </p>
+        </div>
+      )
+  }
+}
+
+
+export default Holyrood
